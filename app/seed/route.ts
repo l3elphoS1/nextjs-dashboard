6444b5cb0845b978bdbfb5cc2719d88a378876d0
l3elphoS1 <110003636+l3elphoS1@users.noreply.github.com>
2025-05-05 16:53:29 +0700

    s

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -115,13 +115,3 @@ export async function GET() {
     return Response.json({ error }, { status: 500 });
   }
 }
-
-export async function fetchRevenue() {
-  try {
-    const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-    return data;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
-  }
-}
