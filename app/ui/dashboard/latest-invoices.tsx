0734eb38d36be0f0d467fb6c7295a8d58979c1fc
l3elphoS1 <110003636+l3elphoS1@users.noreply.github.com>
2025-05-05 16:45:50 +0700

    ss

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
@@ -5,8 +6,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
 import { fetchLatestInvoices } from '@/app/lib/data';
 
-
-
+export default function LatestInvoices({ latestInvoices }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
